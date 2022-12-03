3fe17aa84f2938987656a723bdbdf708390102fc
Mohannadk28 <73769579+Mohannadk28@users.noreply.github.com>
2022-12-03 12:42:55 +0300

    GIMPY :D

--- setup-apps.sh
+++ setup-apps.sh
@@ -2,9 +2,10 @@
 
 PFAPPSDIR=$HOME/.pixelfiles/pixelfiles-apps
 
-rm -rf "$HOME".doom.d
-rm -rf "$HOME".config/aseprite
+rm -rf "$HOME"/.doom.d
+rm -rf "$HOME"/.config/gimp
+rm -rf "$HOME"/.config/aseprite
 
-ln -s "$PFAPPSDIR"/.doom.d "$HOME".doom.d
-ln -s "$PFAPPSDIR"/aseprite "$HOME".config/aseprite
-
+ln -s "$PFAPPSDIR"/.doom.d "$HOME"/.doom.d
+ln -s "$PFAPPSDIR"/gimp "$HOME"/.config/gimp
+ln -s "$PFAPPSDIR"/aseprite "$HOME"/.config/aseprite
