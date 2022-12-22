427f18baedaf51c335d9a6e6ee41002064010ccb
Mohannadk28 <73769579+Mohannadk28@users.noreply.github.com>
2022-12-22 21:13:53 +0300

    d:

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,7 +1,5 @@
 config.load_autoconfig(False)
 
-c.tabs.show = "never"
-
 c.url.searchengines["DEFAULT"] = 'https://search.brave.com/search?q={}'
 c.url.start_pages =              ['https://search.brave.com/']
 c.url.default_page =             'https://search.brave.com/'
